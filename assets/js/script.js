3557f5f9d920df088161d419d02a4f0ad749f1ed
dev-sina <haghanisina3989@gmail.com>
2024-03-23 18:36:00 +0330

    si-faq first commit

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+console.log("Hello world!");
+function toggleAnswerFirst(){
+    const toggleAnswer = document.querySelector('.answer-first');
+    toggleAnswer.classList.toggle('d-block')
+    const minusActive = document.querySelector('.minus-icon-first');
+    minusActive.classList.toggle('d-inline-block')
+    const plusDeactive = document.querySelector('.plus-icon-first');
+    plusDeactive.classList.toggle('d-none')
+}
+function toggleAnswerSecond(){
+    const toggleAnswer = document.querySelector('.answer-second');
+    toggleAnswer.classList.toggle('d-block')
+    const minusActive = document.querySelector('.minus-icon-second');
+    minusActive.classList.toggle('d-inline-block')
+    const plusDeactive = document.querySelector('.plus-icon-second');
+    plusDeactive.classList.toggle('d-none')
+}
+
+
+function toggleAnswerThird(){
+    const toggleAnswer = document.querySelector('.answer-third');
+    toggleAnswer.classList.toggle('d-block')
+    const minusActive = document.querySelector('.minus-icon-third');
+    minusActive.classList.toggle('d-inline-block')
+    const plusDeactive = document.querySelector('.plus-icon-third');
+    plusDeactive.classList.toggle('d-none')
+}
+function toggleAnswerFourth(){
+    const toggleAnswer = document.querySelector('.answer-fourth');
+    toggleAnswer.classList.toggle('d-block')
+    const minusActive = document.querySelector('.minus-icon-fourth');
+    minusActive.classList.toggle('d-inline-block')
+    const plusDeactive = document.querySelector('.plus-icon-fourth');
+    plusDeactive.classList.toggle('d-none')
+}
